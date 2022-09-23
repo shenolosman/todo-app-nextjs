5b47c647da4130391427135dd1bc0ddd2f9f9a35
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-23 20:34:15 +0200

    todo deleting

--- components/Todo.js
+++ components/Todo.js
@@ -1,12 +1,36 @@
-import { ListItem, ListItemText } from "@mui/material";
+import { ListItem, ListItemText, IconButton } from "@mui/material";
 import moment from "moment";
 import "moment/locale/sw";
-
+import { Delete, MoreVert } from "@mui/icons-material";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase/config";
+import { TodoContext } from "../context/TodoContext";
+import { useState, useContext } from "react";
 export default function Todo({ todo }) {
   const { id, title, description, createdDate } = todo;
   // console.log(createdDate);
+  const { showAlert } = useContext(TodoContext);
+  const handleDelete = async (id,e) => {
+    e.preventDefault();
+    const ref = doc(db, "todos", id);
+    await deleteDoc(ref);
+    showAlert("warning", "Todo with "+id+" deleted!");
+  };
   return (
-    <ListItem sx={{ mt: 3, boxShadow: 3 }} style={{ backgroundColor: "#fafafa" }}>
+    <ListItem
+      sx={{ mt: 3, boxShadow: 3 }}
+      style={{ backgroundColor: "#fafafa" }}
+      secondaryAction={
+        <>
+          <IconButton onClick={(e) => handleDelete(id, e)}>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </>
+      }
+    >
       <ListItemText primary={title} secondary={moment(createdDate).format("LLL")} />
     </ListItem>
   );
