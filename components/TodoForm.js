093474ea2436c2fc46ac1fa489b55a0f3ee46847
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-23 20:07:14 +0200

    context and snackbar component

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,20 +1,24 @@
-
 import { Button, TextField, Typography } from "@mui/material";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { db } from "../firebase/config";
+import { TodoContext } from "../context/TodoContext";
 export default function TodoForm() {
   const [todo, setTodo] = useState({ title: "", description: "" });
-
+  const { showAlert } = useContext(TodoContext);
   const handleClick = async (e) => {
     e.preventDefault();
     //console.log(todo)
-    if(todo.title==""||todo.description==""){return}
+    if (todo.title == "" || todo.description == "") {
+        showAlert("error","Title or description can't be empty!")
+      return;
+    }
     const ref = collection(db, "todos");
-    const docRef=await addDoc(ref,{...todo,createdDate:serverTimestamp()})
+    const docRef = await addDoc(ref, { ...todo, createdDate: serverTimestamp() });
     // console.log(docRef.id)
-    setTodo({title:"",description:""})
+    setTodo({ title: "", description: "" });
     // alert(`${todo.title} added!`)
+    showAlert("success", `${todo.title} added!`);
   };
   return (
     <div>
@@ -26,14 +30,16 @@ export default function TodoForm() {
         fullWidth
         label="Title"
         margin="normal"
-        onChange={(e) => setTodo({ ...todo, title: e.target.value })} value={todo.title}
+        onChange={(e) => setTodo({ ...todo, title: e.target.value })}
+        value={todo.title}
       ></TextField>
       <TextField
         fullWidth
         label="Description"
         margin="normal"
         maxRows={3}
-        onChange={(e) => setTodo({ ...todo, description: e.target.value })} value={todo.description}
+        onChange={(e) => setTodo({ ...todo, description: e.target.value })}
+        value={todo.description}
       ></TextField>
 
       <Button sx={{ mt: 3 }} variant="outlined" color="success" onClick={handleClick}>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,59 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import TodoList from "../components/TodoList";
-import TodoForm from "../components/TodoForm"
-import { Container } from "@mui/material";
+import TodoForm from "../components/TodoForm";
+import { Container, Snackbar, Alert } from "@mui/material";
+import { TodoContext } from "../context/TodoContext";
+import { useState } from "react";
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [alertType, setAlertType] = useState("success");
+  const [alertMessage, setAlertMessage] = useState("");
+
+  const showAlert = (type, msg) => {
+    setAlertMessage(msg);
+    setAlertType(type);
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
-    <Container maxWidth="md">
-      <Head>
-        <title>ToDo App</title>
-        <meta name="description" content="NextJs ToDo application" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <TodoContext.Provider value={{ showAlert }}>
+      <Container maxWidth="md">
+        <Head>
+          <title>ToDo App</title>
+          <meta name="description" content="NextJs ToDo application" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <TodoForm />
-        <TodoList />
-      </main>
+        <main className={styles.main}>
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            open={open}
+            autoHideDuration={4000}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity={alertType} sx={{ width: "100%" }}>
+              {alertMessage}
+            </Alert>
+          </Snackbar>
+          <TodoForm />
+          <TodoList />
+        </main>
 
-      <footer className={styles.footer}>
-        <a href="https://shenolosman.github.io/" target="_blank" rel="noopener noreferrer">
-          Shenol Osman
-        </a>
-      </footer>
-    </Container>
+        <footer className={styles.footer}>
+          <a href="https://shenolosman.github.io/" target="_blank" rel="noopener noreferrer">
+            Shenol Osman
+          </a>
+        </footer>
+      </Container>
+    </TodoContext.Provider>
   );
 }
