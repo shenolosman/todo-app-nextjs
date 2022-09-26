457c8702050142f2b7ddda4c618a807b534590d4
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-26 21:28:40 +0200

    inlogging with gmail

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import TodoList from "../components/TodoList";
 import TodoForm from "../components/TodoForm";
-import { Container, Snackbar, Alert } from "@mui/material";
+import { Container, Snackbar, Alert, Typography, Box, Avatar, Button } from "@mui/material";
 import { TodoContext } from "../context/TodoContext";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Loading from "../components/Loading";
 import Login from "../components/Login";
+import { AuthContext } from "../context/AuthContext";
+import { auth } from "../firebase/config";
 
 export default function Home() {
+  const { currentUser } = useContext(AuthContext);
+
   const [open, setOpen] = useState(false);
   const [alertType, setAlertType] = useState("success");
   const [alertMessage, setAlertMessage] = useState("");
@@ -26,14 +30,18 @@ export default function Home() {
     setOpen(false);
   };
   return (
-    <TodoContext.Provider value={{ showAlert,todo,setTodo }}>
+    <TodoContext.Provider value={{ showAlert, todo, setTodo }}>
       <Container maxWidth="md">
         <Head>
           <title>ToDo App</title>
           <meta name="description" content="NextJs ToDo application" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
+        <Box sx={{ display: "flex", justifyContent: "space-between", mt: 3 }}>
+          <Avatar src={currentUser.photoURL}/>
+          <Typography variant="h5">{currentUser.displayName}</Typography>
+          <Button onClick={() => auth.signOut()} variant="contained" color="warning">Logout</Button>
+        </Box>
         <main className={styles.main}>
           <Snackbar
             anchorOrigin={{ vertical: "top", horizontal: "center" }}
